d06317f47d3feafaba091cc7e8c7a4992593604a
Roy <b.roynaim@gmail.com>
2021-03-26 03:41:35 +0100

    js updated

--- main.js
+++ main.js
@@ -1,9 +1,39 @@
 var app = new Vue({
   el: '#root',
   data: {
+    inputText: '',
+    todos: [
+      {
+        title: 'Jogging',
+        status: 'todo'
+      },
 
+      {
+        title: 'Go to KFC',
+        status: 'done'
+      },
+
+      {
+        title: 'Dating with my girlfriend',
+        status: 'done'
+      },
+
+      {
+        title: 'Doing homework',
+        status: 'todo'
+      },
+    ],
   },
   methods: {
+    add: function () {
+      if (this.inputText != '') {
+        let obj = {
+          title: this.inputText
+        }
 
+        this.todos.push(obj);
+        this.inputText = '';
+      }
+    }
   }
 });
