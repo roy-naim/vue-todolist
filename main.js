002e6e615ca4648b6a6aafda4e3c0a918be119c3
Roy <b.roynaim@gmail.com>
2021-04-01 03:47:07 +0200

    Esercizio completato

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var app = new Vue({
   el: '#root',
   data: {
+    currIndex: null,
     inputText: '',
     todos: [
       {
@@ -10,12 +11,12 @@ var app = new Vue({
 
       {
         title: 'Go to KFC',
-        status: 'done'
+        status: 'todo'
       },
 
       {
         title: 'Dating with my girlfriend',
-        status: 'done'
+        status: 'todo'
       },
 
       {
@@ -24,16 +25,47 @@ var app = new Vue({
       },
     ],
   },
+
+  computed: {
+    todosComputed: function () {
+      let todosDone = this.todos.filter((todo) => todo.status == 'done');
+      let todosTodo = this.todos.filter((todo) => todo.status == 'todo');
+      return [...todosTodo, ...todosDone];
+    }
+  },
+
   methods: {
     add: function () {
       if (this.inputText != '') {
-        let obj = {
-          title: this.inputText
+        if (this.currIndex == null) {
+          let obj = {
+            title: this.inputText,
+            status: 'todo'
+          }
+          this.todos.push(obj);
+          
+        } else {
+          this.todos[this.currIndex].title = this.inputText;
+          this.currIndex = null;
         }
-
-        this.todos.push(obj);
         this.inputText = '';
       }
+    },
+
+    check: function (todo) {
+      let index = this.todos.indexOf(todo);
+      this.todos[index].status = 'done';
+    },
+
+    remove: function (todo) {
+      let index = this.todos.indexOf(todo);
+      this.todos.splice(index, 1);
+    },
+
+    edit: function (todo) {
+      let index = this.todos.indexOf(todo);
+      this.inputText = this.todos[index].title;
+      this.currIndex = index;
     }
   }
 });
